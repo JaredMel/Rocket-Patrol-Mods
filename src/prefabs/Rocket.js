2f0b7241bf7b8e95e9bb44234615dc8f0e3e005c
JaredMel <jareddmelendez1764@gmail.com>
2026-01-13 19:57:58 -0800

    Commit 5

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -9,6 +9,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         this.isFiring = false;
         // rocket speed in pixels/frame
         this.moveSpeed = 2;
+        this.sfxShot = scene.sound.add('sfx-shot');
     }
 
     update() {
@@ -21,8 +22,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyFIRE)) {
+        if(Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxShot.play();
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -8,7 +8,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         // store pointValue
         this.points = pointValue;
         // rocket speed in pixels/frame
-        this.moveSpeed = 3;
+        this.moveSpeed = game.settings.spaceshipSpeed;
     }
 
     update() {
